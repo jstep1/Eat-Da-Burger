8f4b8cc1f377b154d216edbd1a8f71ab1c3b2366
Jeremy Stepanovich <jstepanovich91@gmail.com>
2018-03-13 14:03:35 -0500

    added initial files to repository

---
+++ server.js
@@ -0,0 +1,52 @@
+var express = require("express");
+var bodyParser = require("body-parser");
+var methodOverride = require("method-override");
+
+var app = module.exports = express(); 
+
+var cookieParser = require('cookie-parser');
+
+var session = require('express-session');
+//allow sessions
+app.use(session({ secret: 'app', cookie: { maxAge: 6*1000*1000*1000*1000*1000*1000 }}));
+app.use(cookieParser());
+
+// Serve static content for the app from the "public" directory in the application directory.
+app.use(express.static(process.cwd() + "/public"));
+
+app.use(bodyParser.urlencoded({ extended: false }));
+
+// Override with POST having ?_method=DELETE
+app.use(methodOverride("_method"));
+
+// Set Handlebars.
+var exphbs = require("express-handlebars");
+
+var hbs = exphbs.create({
+    // Specify helpers which are only registered on this instance.
+    helpers: {
+        foo: function (a) { return 'FOO!' + a; },
+        bar: function (b) { return 'BAR!' + b; },
+        breaklines: function(text) {
+            text = Handlebars.Utils.escapeExpression(text);
+            text = text.replace(/(\r\n|\n|\r)/gm, '<br>');
+            return new Handlebars.SafeString(text);
+        }
+    },
+    defaultLayout: "main"
+});
+
+app.engine("handlebars", hbs.engine);
+app.set("view engine", "handlebars");
+
+// Import routes and give the server access to them.
+var applicationController = require("./controllers/applicationController.js");
+var burgersController = require("./controllers/burgers_controller.js");
+
+app.use("/", applicationController);
+app.use("/burgers", burgersController);
+
+var port = process.env.PORT || 3000;
+app.listen(port);
+
+
